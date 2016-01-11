1d887fe08779e38912a6b02c458ec3751798e09b
Michael Milton <ttmigueltt@gmail.com>
2016-01-11 07:26:08 +0100

    Brain kind of working

--- src/part1.js
+++ src/part1.js
@@ -2,51 +2,53 @@ import THREE from "three";
 import orbit from 'three-orbit-controls'
 const OrbitControls = orbit(THREE);
 
-//Camera setup
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
-camera.position.z = 1000;
-const controls = new OrbitControls(camera);
-
-//Scene setup
-const scene = new THREE.Scene();
-
 //Renderer setup
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(800, 800);
 document.getElementById("part-1-3d").appendChild(renderer.domElement);
 
+//Camera setup
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
+camera.position.z = 1000;
+const controls = new OrbitControls(camera, renderer.domElement);
+
+//Scene setup
+const scene = new THREE.Scene();
+
 //Meshes
-var coordinates = new THREE.Vector3(0,0,0)
+var coordinates = new THREE.Vector3(0, 0, 0)
 var element = "C"
 
 
-const sphereGeo = new THREE.sphere(coordinates, 1);
-switch (element){
-	case "C":
-		const sphereMaterial = new THREE.MeshBasicMaterial({color: gray});
-		break;
-	case "N":
-		const sphereMaterial = new THREE.MeshBasicMaterial({color: blue});
-		break;
-	case "O":
-		const sphereMaterial = new THREE.MeshBasicMaterial({color: red});
-		break;
-	case "S":
-		const sphereMaterial = new THREE.MeshBasicMaterial({color: yellow});
-		break;
-	case "H":
-		const sphereMaterial = new THREE.MeshBasicMaterial({color: white});
-		break;
-	case default:
-		const sphereMaterial = new THREE.MeshBasicMaterial({color: green});
-		break;
+const sphereGeo = new THREE.SphereGeometry(100);
+var sphereMaterial;
+switch (element) {
+    case "C":
+        sphereMaterial = new THREE.MeshBasicMaterial({color: "gray"});
+        break;
+    case "N":
+        sphereMaterial = new THREE.MeshBasicMaterial({color: "blue"});
+        break;
+    case "O":
+        sphereMaterial = new THREE.MeshBasicMaterial({color: "red"});
+        break;
+    case "S":
+        sphereMaterial = new THREE.MeshBasicMaterial({color: "yellow"});
+        break;
+    case "H":
+        sphereMaterial = new THREE.MeshBasicMaterial({color: "white"});
+        break;
+    default:
+        sphereMaterial = new THREE.MeshBasicMaterial({color: "green"});
+        break;
 }
-const sphereObj = new THREE.Mesh(sphereGeo, sphereObj);
+const sphereObj = new THREE.Mesh(sphereGeo, sphereMaterial);
+window.sphere = sphereObj;
 scene.add(sphereObj);
 
 //Render loop
 function render() {
-    requestAnimationFrame( render );
-    renderer.render( scene, camera );
+    requestAnimationFrame(render);
+    renderer.render(scene, camera);
 }
 render();
--- src/part2.js
+++ src/part2.js
@@ -1,52 +1,104 @@
+//Imports
 import THREE from "three";
 import orbit from 'three-orbit-controls'
+import pad from 'pad';
 const OrbitControls = orbit(THREE);
+
+//Constants
+const dimemsions = {
+    x: 800,
+    y: 800
+};
+const images = {
+    axial: {
+        prefix: "/images/axial_stack/slice_",
+        count: 182,
+        currentImage: 91
+    },
+    coronal: {
+        prefix: "/images/coronal_stack/slice_",
+        count: 182,
+        currentImage: 91
+    },
+    sagittal: {
+        prefix: "/images/saggital_stack/slice_",
+        count: 218,
+        currentImage: 109
+    }
+};
+
+function getSliceString(section, number){
+    return `/images/${section}_stack/slice_${pad(3, number, '0')}.png`;
+}
+
+//Listen for input changes
+document.getElementById("range-x").addEventListener('input', function () {
+    setSlice('axial', this.value);
+});
+document.getElementById("range-y").addEventListener('input', function () {
+    setSlice('coronal', this.value);
+});
+document.getElementById("range-z").addEventListener('input', function () {
+    setSlice('sagittal', this.value);
+});
+
+function setSlice(section, slice) {
+    images[section].currentImage = slice;
+    images[section].plane.material.map = THREE.ImageUtils.loadTexture(getSliceString(section, slice));
+    images[section].plane.material.needsUpdate = true;
+}
+
+//Renderer setup
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(dimemsions.x, dimemsions.y);
+document.getElementById("part-2-3d").appendChild(renderer.domElement);
 
 //Camera setup
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
 camera.position.z = 1000;
-const controls = new OrbitControls(camera);
+const controls = new OrbitControls(camera, renderer.domElement);
 
 //Scene setup
 const scene = new THREE.Scene();
 
-//Renderer setup
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(800, 800);
-document.getElementById("part-2-3d").appendChild(renderer.domElement);
-
-////Axes
-//const axes = new THREE.AxisHelper(20);
-//scene.add(axes);
-
 //Meshes
 const xPlane = new THREE.Mesh(
     new THREE.PlaneGeometry(800, 800),
-    new THREE.MeshBasicMaterial({color: 0xff0000})
+    new THREE.MeshBasicMaterial()
 );
+xPlane.material.side = THREE.DoubleSide;
+images.axial.plane = xPlane;
+
 const yPlane = new THREE.Mesh(
     new THREE.PlaneGeometry(800, 800),
-    new THREE.MeshBasicMaterial({color: 0x00ff00})
+    new THREE.MeshBasicMaterial()
 );
-yPlane.rotateX(Math.PI/2);
+yPlane.rotateX(Math.PI / 2);
+yPlane.material.side = THREE.DoubleSide;
+images.coronal.plane = yPlane;
+
 const zPlane = new THREE.Mesh(
     new THREE.PlaneGeometry(800, 800),
-    new THREE.MeshBasicMaterial({color: 0x0000ff})
+    new THREE.MeshBasicMaterial()
 );
-zPlane.rotateY(Math.PI/2);
+zPlane.rotateY(Math.PI / 2);
+zPlane.material.side = THREE.DoubleSide;
+images.sagittal.plane = zPlane;
+
 scene.add(xPlane, yPlane, zPlane);
 
-xPlane.material.side = THREE.DoubleSide;
-yPlane.material.side = THREE.DoubleSide;
-zPlane.material.side = THREE.DoubleSide;
+//Set initial textures
+for (let section in images)
+    setSlice(section, images[section].currentImage);
 
-window.xPlane = xPlane;
-window.yPlane = yPlane;
-window.zPlane = zPlane;
+//Preload textures
+for (let section in images)
+    for (let i = 0; i < images[section].count; i++)
+        THREE.ImageUtils.loadTexture(getSliceString(section, i));
 
 //Render loop
 function render() {
-    requestAnimationFrame( render );
-    renderer.render( scene, camera );
+    requestAnimationFrame(render);
+    renderer.render(scene, camera);
 }
 render();
