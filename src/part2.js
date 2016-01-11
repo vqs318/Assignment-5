05e681a7f3bf650260bd361e940a6778071a9b61
Michael Milton <ttmigueltt@gmail.com>
2016-01-11 03:04:41 +0100

    Plane!

--- src/part2.js
+++ src/part2.js
@@ -1,8 +1,11 @@
 import THREE from "three";
+import orbit from 'three-orbit-controls'
+const OrbitControls = orbit(THREE);
 
 //Camera setup
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
 camera.position.z = 1000;
+const controls = new OrbitControls(camera);
 
 //Scene setup
 const scene = new THREE.Scene();
@@ -10,10 +13,17 @@ const scene = new THREE.Scene();
 //Renderer setup
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(800, 800);
+document.getElementById("part-2-3d").appendChild(renderer.domElement);
 
-const planeGeometry = new THREE.PlaneGeometry(60, 20);
-const planeMaterial = new THREE.MeshBasicMaterial({color: 0xff0000, wireframe: true});
+//Meshes
+const planeGeometry = new THREE.PlaneGeometry(800, 800);
+const planeMaterial = new THREE.MeshBasicMaterial({color: 0xff0000});
 const plane = new THREE.Mesh(planeGeometry, planeMaterial);
 scene.add(plane);
 
-document.getElementById("part-2-3d").appendChild(renderer.domElement);
+//Render loop
+function render() {
+    requestAnimationFrame( render );
+    renderer.render( scene, camera );
+}
+render();
